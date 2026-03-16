dcdcc8fad6b797a4677fd7d1235f84a3acef878f
deepakn-0603 <deepak.n.0603@gmail.com>
2026-03-16 18:47:30 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        self.wfile.write(b"<h1>The webpage running using automated by jenkins</h1>")
+        self.wfile.write(b"<h1>The webpage running using automated by jenkins</h1><br><h2>The end...</h2>")
 server = HTTPServer(("0.0.0.0", 8000), MyHandler)
 print("Server running at http://localhost:8000")
 
